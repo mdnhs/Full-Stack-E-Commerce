87791d34fa5a188fc468a0582f16d2ffe6e3e2f7
Lostovayne <moises.barrera.dev@gmail.com>
2024-11-20 02:59:36 -0300

    building a backend client

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,16 @@
-import { createClient } from 'next-sanity'
+import { createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId } from '../env'
+import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
+  stega: {
+    // https://www.sanity.io/docs/stega Editor de la web en vivo
+    studioUrl: process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/studio`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/studio`,
+  },
+});
