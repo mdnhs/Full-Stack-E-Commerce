5d3fe52ff867f31d98efb3642f99562017317917
Lostovayne <moises.barrera.dev@gmail.com>
2024-12-03 02:41:33 -0300

    agregada la funcion del boton de agregar producto al carrito

--- app/(store)/product/[slug]/page.tsx
+++ app/(store)/product/[slug]/page.tsx
@@ -1,3 +1,5 @@
+import AddToBasketButton from "@/components/AddToBasketButton";
+import { Button } from "@/components/ui/button";
 import { imageUrl } from "@/lib/imageUrl";
 import { getProductBySlug } from "@/sanity/lib/products/getProductBySlug";
 import { PortableText } from "next-sanity";
@@ -43,6 +45,10 @@ const ProductPage = async ({ params }: { params: Promise<{ slug: string }> }) =>
             <div className=" prose max-w-none mb-6">
               {Array.isArray(product.description) && <PortableText value={product.description} />}
             </div>
+          </div>
+          <div className="mt-6">
+            <AddToBasketButton product={product} disabled={isOutOfStock} />
+            <Button>Add to Basket</Button>
           </div>
         </div>
       </div>
---
+++ components/AddToBasketButton.tsx
@@ -0,0 +1,42 @@
+"use client";
+import type { Product } from "@/sanity.types";
+import useBasketStore from "@/store";
+import { FC, ReactElement, useEffect, useState } from "react";
+
+interface AddToBasketButtonProps {
+  // Prop types here
+  product: Product;
+  disabled?: boolean;
+}
+
+const AddToBasketButton: FC<AddToBasketButtonProps> = ({ product, disabled }): ReactElement => {
+  const { addItem, removeItem, getItemCount } = useBasketStore();
+  const itemCount = getItemCount(product._id);
+  const [isClient, setIsClient] = useState<boolean>(false);
+
+  useEffect(() => setIsClient(true), []);
+
+  return (
+    <div className="flex items-center justify-center space-x-2">
+      <button
+        onClick={() => removeItem(product._id)}
+        className={`size-8 rounded-full flex items-center justify-center transition-colors duration-200 ${itemCount === 0 ? "bg-gray-100 cursor-not-allowed" : "bg-gray-200 hover:bg-gray-300"}`}
+        disabled={itemCount === 0 || disabled}
+      >
+        <span className={`text-xl font-bold ${itemCount === 0 ? "text-gray-400" : "text-gray-600"} `}>-</span>
+      </button>
+      <span className="w-8 text-center font-semibold">{itemCount}</span>
+      <button
+        onClick={() => addItem(product)}
+        className={`size-8 rounded-full flex items-center justify-center transition-colors duration-200 ${
+          disabled ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500 hover:bg-gray-600"
+        }`}
+        disabled={disabled}
+      >
+        <span className="text-xl font-bold text-white">+</span>
+      </button>
+    </div>
+  );
+};
+
+export default AddToBasketButton;
